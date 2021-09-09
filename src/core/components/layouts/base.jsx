741b05df3b7a81c419d840b684440f41e80eda14
Pramod-1246 <80253801+Pramod-1246@users.noreply.github.com>
2021-09-09 12:07:52 +0530

    added div to class to apply image

--- src/core/components/layouts/base.jsx
+++ src/core/components/layouts/base.jsx
@@ -90,7 +90,7 @@ export default class BaseLayout extends React.Component {
           <SvgAssets />
           <VersionPragmaFilter isSwagger2={isSwagger2} isOAS3={isOAS3} alsoShow={<Errors/>}>
             <Errors/>
-            <div>
+            <div className="banner-image">
             <Row className="information-container">
               <Col mobile={12}>
                 <InfoContainer/>
